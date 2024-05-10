869e66ae11a1e08521ebb4b518a7b83501186dad
Eric Lluis <ericlluis84@gmail.com>
2024-05-10 16:42:35 +0200

    Create generalError test

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,4 +4,5 @@ module.exports = {
   testEnvironment: "node",
   resolver: "jest-ts-webcompat-resolver",
   testMatch: ["**/src/**/*.test.ts"],
+  collectCoverageFrom: ["**/*.{ts,tsx}", "!**/index.ts/**", "!**/app.ts/**"],
 };

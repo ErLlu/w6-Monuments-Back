aee094b440e2ab0aa11776eb6a6e0a3a2b37114c
Eric Lluis <ericlluis84@gmail.com>
2024-05-11 19:13:22 +0200

    Add MonumentStructure and MonumentLocution types

---
+++ src/monuments/types.ts
@@ -0,0 +1,13 @@
+export interface MonumentStructure {
+  name: string;
+  description: string;
+  imageUrl: string;
+  country: string;
+  city: string;
+  id: string;
+}
+
+export interface MonumentLocation {
+  city: string;
+  country: string;
+}

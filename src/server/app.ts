43a56e24d9dac4d0b284bd9bc975db97b21120c1
Eric Lluis <ericlluis84@gmail.com>
2024-05-12 22:32:22 +0200

    Create monuments router

--- src/server/app.ts
+++ src/server/app.ts
@@ -5,12 +5,15 @@ import {
 } from "./errors/middlewares/errorsMiddlewares.js";
 import app from "./index.js";
 import PingController from "./PingController/PingController.js";
+import monumentsRouter from "../monuments/router/monumentsRouter.js";
 
 app.use(morgan("dev"));
 
 const pingController = new PingController();
 
 app.get("/", pingController.getPong);
+
+app.get("/monuments", monumentsRouter);
 
 app.use(notFoundError);
 app.use(generalError);
